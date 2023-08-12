1c22860b833053bfefcf0702a866e19ce659dba6
Joel Macht <joel.macht@student.kit.edu>
2023-08-12 11:39:17 +0200

    Setup IDE with Intellisense and build automation
    Init hardware and set black backdrop on main

--- app.c
+++ app.c
@@ -1,6 +1,26 @@
-#include <nds.h>
+#include "nds/arm9/console.h"
+#include "nds/system.h"
+
+#include <stdio.h>
 
 int main(void)
 {
+    powerOn(POWER_ALL_2D);
+
+    videoSetMode(MODE_0_2D);
+
+    setBackdropColor(1);
+
+    // PrintConsole console;
+    // consoleInit(&console, 0, BgType_Text8bpp, BgSize_B16_256x256, 0, 0, false, true);
+
+    // consoleDebugInit(DebugDevice_CONSOLE);
+    // consoleDemoInit();
+
+    while(true)
+    {
+        // printf("Hello World\n");
+    }
+
     return 0;
 }
