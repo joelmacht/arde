321f57da3d74e226f82cd4e451dc26d3ca4b4cc1
Joel Macht <joel.macht@student.kit.edu>
2023-08-28 21:22:20 +0200

    Introduce coordinate spaces
    Run physics in world space
    
    Make sure to not draw outside framebuffer memory

--- app.c
+++ app.c
@@ -11,19 +11,96 @@
 #define FRAMEBUFFER_SIZE SCREEN_WIDTH * SCREEN_HEIGHT
 static u16 FRAMEBUFFER[FRAMEBUFFER_SIZE];
 
+typedef struct {
+    float data[2];
+} vector_t;
+
+typedef struct {
+    float data[4];
+} matrix_t;
+
+typedef struct {
+    matrix_t rotation;
+    vector_t scaling;
+    vector_t translation;
+} transform_t;
+
+void vector_add(const vector_t* first, const vector_t* second, vector_t* result)
+{
+    result->data[0] = first->data[0] + second->data[0];
+    result->data[1] = first->data[1] + second->data[1];
+}
+
+void vector_multiply(const vector_t* first, const vector_t* second, vector_t* result)
+{
+    result->data[0] = first->data[0] * second->data[0];
+    result->data[1] = first->data[1] * second->data[1];
+}
+
+void matrix_vector_multiply(const matrix_t* matrix, const vector_t* vector, vector_t* result)
+{
+    result->data[0] = vector->data[0] * matrix->data[0] + vector->data[1] * matrix->data[1];
+    result->data[1] = vector->data[0] * matrix->data[2] + vector->data[1] * matrix->data[3];
+}
+
+void transform_position(const transform_t* transform, const vector_t* position, vector_t* result)
+{
+    matrix_vector_multiply(&transform->rotation, position, result);
+    vector_t scratchboard;
+    vector_multiply(&transform->scaling, result, &scratchboard);
+    vector_add(&transform->translation, &scratchboard, result);
+}
+
+void transform_direction(const transform_t* transform, const vector_t* direction, vector_t* result)
+{
+    vector_t scratchboard;
+    matrix_vector_multiply(&transform->rotation, direction, &scratchboard);
+    vector_multiply(&transform->scaling, &scratchboard, result);
+}
+
 void clear_framebuffer()
 {
     memset(FRAMEBUFFER, (u16)0, FRAMEBUFFER_SIZE * sizeof(FRAMEBUFFER[0]));
 }
 
+const transform_t world_to_observer = {
+    {{1.0f, 0.0f, 0.0f, 1.0f}},
+    {{1.0f, 1.0f}},
+    {{0.0f, 0.0f}}
+};
+
+const transform_t observer_to_sensor = {
+    {{1.0f, 0.0f, 0.0f, -1.0f}},
+    {{SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f}},
+    {{SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f}}
+};
+
 void draw_pixel(int x, int y, u8 r, u8 g, u8 b)
 {
-    FRAMEBUFFER[y * SCREEN_WIDTH + x] = ARGB16(1, r >> 3, g >> 3, b >> 3);
+    if ((0 <= x) && (x < SCREEN_WIDTH) && (0 <= y) && (y < SCREEN_HEIGHT))
+    {
+        FRAMEBUFFER[y * SCREEN_WIDTH + x] = ARGB16(1, r >> 3, g >> 3, b >> 3);
+    }
 }
 
 void draw_circle(float x, float y, float radius)
 {
+    vector_t position_world = {{x, y}};
+    vector_t position_observation;
+    vector_t position_sensor;
+    transform_position(&world_to_observer, &position_world, &position_observation);
+    transform_position(&observer_to_sensor, &position_observation, &position_sensor);
+    x = position_sensor.data[0];
+    y = position_sensor.data[1];
+    
+    position_world.data[0] = 0.0f;
+    position_world.data[1] = radius;
+    transform_direction(&world_to_observer, &position_world, &position_observation);
+    transform_direction(&observer_to_sensor, &position_observation, &position_sensor);
+    radius = position_sensor.data[1];
+
     radius = radius > 0.0f ? radius : -radius;
+
     float radius_squared = radius * radius;
 
     int min_i = (int)floorf(x - radius);
@@ -55,7 +132,7 @@ typedef struct
 
 void update_acceleration(point_mass_t* point_mass, int point_mass_count, point_mass_t* point_masses)
 {
-    static const float constant = 1e4f;
+    static const float constant = 1e0f;
 
     float force[2];
     force[0] = 0.0f;
@@ -104,7 +181,7 @@ void point_mass_draw_list(int point_mass_count, point_mass_t* point_masses)
     for (int point_mass_index = 0; point_mass_index < point_mass_count; ++point_mass_index)
     {
         point_mass_t* point_mass = point_masses + point_mass_index;
-        draw_circle(point_mass->position[0], point_mass->position[1], point_mass->mass);
+        draw_circle(point_mass->position[0], point_mass->position[1], 0.01f * point_mass->mass);
     }
 }
 
@@ -120,30 +197,30 @@ int main(void)
     point_mass_t point_masses[point_mass_count];
     
     point_masses[0].mass = 5.0;
-    point_masses[0].position[0] = SCREEN_WIDTH / 2.0f - 50.0f;
-    point_masses[0].position[1] = SCREEN_HEIGHT / 2.0f;
-    point_masses[0].velocity[0] = 10.0f;
-    point_masses[0].velocity[1] = 20.0f;
+    point_masses[0].position[0] = 0.5f;
+    point_masses[0].position[1] = 0.0f;
+    point_masses[0].velocity[0] = -0.5f;
+    point_masses[0].velocity[1] = 1.0f;
     point_masses[0].acceleration[0] = 0.0f;
     point_masses[0].acceleration[1] = 0.0f;
 
     point_masses[1].mass = 5.0;
-    point_masses[1].position[0] = SCREEN_WIDTH / 2.0f + 50.0f;
-    point_masses[1].position[1] = SCREEN_HEIGHT / 2.0f;
-    point_masses[1].velocity[0] = -40.0f;
-    point_masses[1].velocity[1] = -20.0f;
+    point_masses[1].position[0] = -0.5f;
+    point_masses[1].position[1] = 0.0f;
+    point_masses[1].velocity[0] = 2.0f;
+    point_masses[1].velocity[1] = -2.0f;
     point_masses[1].acceleration[0] = 0.0f;
     point_masses[1].acceleration[1] = 0.0f;
 
-    point_masses[2].mass = 30.0;
-    point_masses[2].position[0] = SCREEN_WIDTH / 2.0f;
-    point_masses[2].position[1] = SCREEN_HEIGHT / 2.0f;
+    point_masses[2].mass = 8.0;
+    point_masses[2].position[0] = 0.0f;
+    point_masses[2].position[1] = 0.0f;
     point_masses[2].velocity[0] = 0.0f;
     point_masses[2].velocity[1] = 0.0f;
     point_masses[2].acceleration[0] = 0.0f;
     point_masses[2].acceleration[1] = 0.0f;
 
-    float timestep = 1e-2f;
+    const float timestep = 1e-3f;
 
     while(1)
     {
