d2c3825eebcac0b1fbe0257e7ca56cf586236e27
Joel Macht <joel.macht@student.kit.edu>
2023-09-06 23:59:19 +0200

    Change project name
    Prefix api with project name

--- app.c
+++ app.c
@@ -14,69 +14,69 @@ static u16 FRAMEBUFFER[FRAMEBUFFER_SIZE];
 
 typedef struct {
     float data[2];
-} vector_t;
+} arde_vector_t;
 
 typedef struct {
     float data[4];
-} matrix_t;
+} arde_matrix_t;
 
 typedef struct {
-    matrix_t rotation;
-    vector_t scaling;
-    vector_t translation;
-} transform_t;
+    arde_matrix_t rotation;
+    arde_vector_t scaling;
+    arde_vector_t translation;
+} arde_transform_t;
 
-void vector_add(const vector_t* first, const vector_t* second, vector_t* result)
+void arde_vector_add(const arde_vector_t* first, const arde_vector_t* second, arde_vector_t* result)
 {
     result->data[0] = first->data[0] + second->data[0];
     result->data[1] = first->data[1] + second->data[1];
 }
 
-void vector_multiply(const vector_t* first, const vector_t* second, vector_t* result)
+void arde_vector_multiply(const arde_vector_t* first, const arde_vector_t* second, arde_vector_t* result)
 {
     result->data[0] = first->data[0] * second->data[0];
     result->data[1] = first->data[1] * second->data[1];
 }
 
-void matrix_vector_multiply(const matrix_t* matrix, const vector_t* vector, vector_t* result)
+void arde_matrix_vector_multiply(const arde_matrix_t* matrix, const arde_vector_t* vector, arde_vector_t* result)
 {
     result->data[0] = vector->data[0] * matrix->data[0] + vector->data[1] * matrix->data[1];
     result->data[1] = vector->data[0] * matrix->data[2] + vector->data[1] * matrix->data[3];
 }
 
-void transform_position(const transform_t* transform, const vector_t* position, vector_t* result)
+void arde_transform_position(const arde_transform_t* transform, const arde_vector_t* position, arde_vector_t* result)
 {
-    matrix_vector_multiply(&transform->rotation, position, result);
-    vector_t scratchboard;
-    vector_multiply(&transform->scaling, result, &scratchboard);
-    vector_add(&transform->translation, &scratchboard, result);
+    arde_matrix_vector_multiply(&transform->rotation, position, result);
+    arde_vector_t scratchboard;
+    arde_vector_multiply(&transform->scaling, result, &scratchboard);
+    arde_vector_add(&transform->translation, &scratchboard, result);
 }
 
-void transform_direction(const transform_t* transform, const vector_t* direction, vector_t* result)
+void arde_transform_direction(const arde_transform_t* transform, const arde_vector_t* direction, arde_vector_t* result)
 {
-    vector_t scratchboard;
-    matrix_vector_multiply(&transform->rotation, direction, &scratchboard);
-    vector_multiply(&transform->scaling, &scratchboard, result);
+    arde_vector_t scratchboard;
+    arde_matrix_vector_multiply(&transform->rotation, direction, &scratchboard);
+    arde_vector_multiply(&transform->scaling, &scratchboard, result);
 }
 
-void clear_framebuffer()
+void arde_clear_framebuffer()
 {
     memset(FRAMEBUFFER, (u16)0, FRAMEBUFFER_SIZE * sizeof(FRAMEBUFFER[0]));
 }
 
-const transform_t world_to_observer = {
+const arde_transform_t world_to_observer = {
     {{1.0f, 0.0f, 0.0f, 1.0f}},
     {{1.0f, 1.0f}},
     {{0.0f, 0.0f}}
 };
 
-const transform_t observer_to_sensor = {
+const arde_transform_t observer_to_sensor = {
     {{1.0f, 0.0f, 0.0f, -1.0f}},
     {{SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f}},
     {{SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f}}
 };
 
-void draw_pixel(int x, int y, u8 r, u8 g, u8 b)
+void arde_draw_pixel(int x, int y, u8 r, u8 g, u8 b)
 {
     if ((0 <= x) && (x < SCREEN_WIDTH) && (0 <= y) && (y < SCREEN_HEIGHT))
     {
@@ -84,20 +84,20 @@ void draw_pixel(int x, int y, u8 r, u8 g, u8 b)
     }
 }
 
-void draw_circle(float x, float y, float radius)
+void arde_draw_circle(float x, float y, float radius)
 {
-    vector_t position_world = {{x, y}};
-    vector_t position_observation;
-    vector_t position_sensor;
-    transform_position(&world_to_observer, &position_world, &position_observation);
-    transform_position(&observer_to_sensor, &position_observation, &position_sensor);
+    arde_vector_t position_world = {{x, y}};
+    arde_vector_t position_observation;
+    arde_vector_t position_sensor;
+    arde_transform_position(&world_to_observer, &position_world, &position_observation);
+    arde_transform_position(&observer_to_sensor, &position_observation, &position_sensor);
     x = position_sensor.data[0];
     y = position_sensor.data[1];
     
     position_world.data[0] = 0.0f;
     position_world.data[1] = radius;
-    transform_direction(&world_to_observer, &position_world, &position_observation);
-    transform_direction(&observer_to_sensor, &position_observation, &position_sensor);
+    arde_transform_direction(&world_to_observer, &position_world, &position_observation);
+    arde_transform_direction(&observer_to_sensor, &position_observation, &position_sensor);
     radius = position_sensor.data[1];
 
     radius = radius > 0.0f ? radius : -radius;
@@ -117,7 +117,7 @@ void draw_circle(float x, float y, float radius)
             float distance_squared = (pixel_center[0] - x) * (pixel_center[0] - x) + (pixel_center[1] - y) * (pixel_center[1] - y);
             if (distance_squared <= radius_squared)
             {
-                draw_pixel(i, j, 255, 255, 255);
+                arde_draw_pixel(i, j, 255, 255, 255);
             }
         }
     }
@@ -129,9 +129,9 @@ typedef struct
     float position[2];
     float velocity[2];
     float acceleration[2];
-} point_mass_t;
+} arde_point_mass_t;
 
-void update_acceleration(point_mass_t* point_mass, int point_mass_count, point_mass_t* point_masses)
+void arde_update_acceleration(arde_point_mass_t* point_mass, int point_mass_count, arde_point_mass_t* point_masses)
 {
     static const float constant = 1e0f;
 
@@ -141,7 +141,7 @@ void update_acceleration(point_mass_t* point_mass, int point_mass_count, point_m
 
     for (int point_mass_index = 0; point_mass_index < point_mass_count; ++point_mass_index)
     {
-        point_mass_t* other_point_mass = point_masses + point_mass_index;
+        arde_point_mass_t* other_point_mass = point_masses + point_mass_index;
         
         if (point_mass != other_point_mass)
         {
@@ -160,16 +160,16 @@ void update_acceleration(point_mass_t* point_mass, int point_mass_count, point_m
     point_mass->acceleration[1] = force[1] / point_mass->mass;
 }
 
-void point_mass_update_list(int point_mass_count, point_mass_t* point_masses, float timestep)
+void arde_point_mass_update_list(int point_mass_count, arde_point_mass_t* point_masses, float timestep)
 {
     for (int point_mass_index = 0; point_mass_index < point_mass_count; ++point_mass_index)
     {
-        update_acceleration(point_masses + point_mass_index, point_mass_count, point_masses);
+        arde_update_acceleration(point_masses + point_mass_index, point_mass_count, point_masses);
     }
 
     for (int point_mass_index = 0; point_mass_index < point_mass_count; ++point_mass_index)
     {
-        point_mass_t* point_mass = point_masses + point_mass_index;
+        arde_point_mass_t* point_mass = point_masses + point_mass_index;
         point_mass->velocity[0] += timestep * point_mass->acceleration[0];
         point_mass->velocity[1] += timestep * point_mass->acceleration[1];
         point_mass->position[0] += timestep * point_mass->velocity[0];
@@ -177,12 +177,12 @@ void point_mass_update_list(int point_mass_count, point_mass_t* point_masses, fl
     }
 }
 
-void point_mass_draw_list(int point_mass_count, point_mass_t* point_masses)
+void arde_point_mass_draw_list(int point_mass_count, arde_point_mass_t* point_masses)
 {
     for (int point_mass_index = 0; point_mass_index < point_mass_count; ++point_mass_index)
     {
-        point_mass_t* point_mass = point_masses + point_mass_index;
-        draw_circle(point_mass->position[0], point_mass->position[1], 0.01f);
+        arde_point_mass_t* point_mass = point_masses + point_mass_index;
+        arde_draw_circle(point_mass->position[0], point_mass->position[1], 0.01f);
     }
 }
 
@@ -195,11 +195,11 @@ int main(void)
     consoleDemoInit();
 
     const int point_mass_count = 50;
-    point_mass_t point_masses[point_mass_count];
+    arde_point_mass_t point_masses[point_mass_count];
 
     for (int point_mass_index = 0; point_mass_index < point_mass_count; ++point_mass_index)
     {
-        point_mass_t* point_mass = point_masses + point_mass_index;
+        arde_point_mass_t* point_mass = point_masses + point_mass_index;
         point_mass->mass = 1.0f;
         point_mass->position[0] = 2.0f * rand()/RAND_MAX - 1.0f;
         point_mass->position[1] = 2.0f * rand()/RAND_MAX - 1.0f;
@@ -215,11 +215,11 @@ int main(void)
     {
         cpuStartTiming(0);
 
-        point_mass_update_list(point_mass_count, point_masses, timestep);
+        arde_point_mass_update_list(point_mass_count, point_masses, timestep);
 
-        clear_framebuffer();
+        arde_clear_framebuffer();
 
-        point_mass_draw_list(point_mass_count, point_masses);
+        arde_point_mass_draw_list(point_mass_count, point_masses);
 
         u32 per_frame_ticks = cpuEndTiming();
 
