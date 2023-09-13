a03b2034a47b28c38c37f109b87e2440d37fd186
Joel Macht <joel.macht@student.kit.edu>
2023-09-13 23:12:23 +0200

    Reduce complexity by exploiting force antisymmetry

--- source/arde/physics/gravity/gravity.c
+++ source/arde/physics/gravity/gravity.c
@@ -16,32 +16,25 @@ float arde_radial_harmonic_potential_gradient(float mass, float distance)
 
 void arde_update_acceleration(arde_point_mass_t* point_mass, int point_mass_count, arde_point_mass_t* point_masses)
 {
-    float force[2];
-    force[0] = 0.0f;
-    force[1] = 0.0f;
+    // required to happen before
+    // point_mass->acceleration[0] = 0.0f;
+    // point_mass->acceleration[1] = 0.0f;
 
-    for (int point_mass_index = 0; point_mass_index < point_mass_count; ++point_mass_index)
+    for (arde_point_mass_t* other_point_mass = point_mass + 1; other_point_mass <= point_masses + point_mass_count - 1; ++other_point_mass)
     {
-        arde_point_mass_t* other_point_mass = point_masses + point_mass_index;
-        
-        if (point_mass != other_point_mass)
-        {
-            float radial_direction[2] = {
-                point_mass->position[0] - other_point_mass->position[0],
-                point_mass->position[1] - other_point_mass->position[1],
-            };
-            float distance = sqrtf(radial_direction[0] * radial_direction[0] + radial_direction[1] * radial_direction[1]);
-            radial_direction[0] /= distance;
-            radial_direction[1] /= distance;
-            // float force_magnitude = point_mass->mass * -arde_gravitational_potential_gradient(other_point_mass->mass, distance);
-            float force_magnitude = point_mass->mass * -arde_radial_harmonic_potential_gradient(other_point_mass->mass, distance);
-            force[0] += force_magnitude * radial_direction[0];
-            force[1] += force_magnitude * radial_direction[1];
-        }
+        float radial_direction[2] = {
+            point_mass->position[0] - other_point_mass->position[0],
+            point_mass->position[1] - other_point_mass->position[1],
+        };
+        float distance = sqrtf(radial_direction[0] * radial_direction[0] + radial_direction[1] * radial_direction[1]);
+        radial_direction[0] /= distance;
+        radial_direction[1] /= distance;
+        float acceleration_magnitude = -arde_radial_harmonic_potential_gradient(other_point_mass->mass, distance);
+        point_mass->acceleration[0] += acceleration_magnitude * radial_direction[0];
+        point_mass->acceleration[1] += acceleration_magnitude * radial_direction[1];
+        other_point_mass->acceleration[0] -= acceleration_magnitude * radial_direction[0];
+        other_point_mass->acceleration[1] -= acceleration_magnitude * radial_direction[1];
     }
-
-    point_mass->acceleration[0] = force[0] / point_mass->mass;
-    point_mass->acceleration[1] = force[1] / point_mass->mass;
 }
 
 void arde_point_mass_update_list(int point_mass_count, arde_point_mass_t* point_masses, float timestep)
@@ -58,6 +51,9 @@ void arde_point_mass_update_list(int point_mass_count, arde_point_mass_t* point_
         point_mass->velocity[1] += timestep * point_mass->acceleration[1];
         point_mass->position[0] += timestep * point_mass->velocity[0];
         point_mass->position[1] += timestep * point_mass->velocity[1];
+        // reset
+        point_mass->acceleration[0] = 0.0f;
+        point_mass->acceleration[1] = 0.0f;
     }
 }
 
